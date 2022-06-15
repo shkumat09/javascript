db1ce5a8fcf714104567673178f3f2abd0aeed51
shkumat09 <shkumat09@gmail.com>
2022-06-15 20:23:28 +0500

    end modul

---
+++ secondmodul/boolean.js
@@ -0,0 +1,19 @@
+let yep = true;
+let nope = false;
+
+let yes = 5 > 3;
+let no = 5 < 3;
+
+//инвертирование
+let alsoNo = !yes;
+
+let x = 10;
+let y = 20;
+
+if (x > y) {
+  console.log('x больше y');
+}
+
+if (x < y) {
+  console.log('x меньше y');
+}
---
+++ secondmodul/spesial-chars.js
@@ -0,0 +1,8 @@
+console.log('"Кавычка в кавычках: \'"');
+console.log("'Кавычка в кавычках: \"'");
+console.log(`'Кавычка в кавычках: \`'`);
+console.log('Символ табуляции: \t');
+console.log('Обратный слеш: \\');
+console.log('\[31');//цифра 1 в UTF-8 в hex
+console.log('\u0031');//цифра 1 в UTF-16 в hex
+console.log('\u{1F354}');//Символ эмодзи гамбургер, код в UTF-32 в hex
